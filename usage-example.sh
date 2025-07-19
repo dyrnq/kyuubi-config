12920b74395ec81777c1d4cd3f23df4dd5ec72df
dyrnq <dyrnq@qq.com>
2025-07-19 12:44:48 +0800

    Update usage-example.sh

--- usage-example.sh
+++ usage-example.sh
@@ -3,9 +3,9 @@
 
 for kv in "v1.10.1" "v1.10.2" "v1.9.1" "v1.9.2" "v1.9.3" "v1.9.4" "v1.8.3" "master"; do
 mkdir -p kyuubi/$kv
-java -jar target/kyuubi-config.jar config -ver $kv --format json               > kyuubi/$kv/kyuubi-config.json
-java -jar target/kyuubi-config.jar config -ver $kv --format json --no-section  > kyuubi/$kv/kyuubi-config-no-section.json
-java -jar target/kyuubi-config.jar config -ver $kv --format conf               > kyuubi/$kv/kyuubi-config.conf
+java -jar target/kyuubi-config.jar kyuubi -ver $kv --format json               > kyuubi/$kv/kyuubi-config.json
+java -jar target/kyuubi-config.jar kyuubi -ver $kv --format json --no-section  > kyuubi/$kv/kyuubi-config-no-section.json
+java -jar target/kyuubi-config.jar kyuubi -ver $kv --format conf               > kyuubi/$kv/kyuubi-config.conf
 done
 
 for kv in "3.5.4" "3.5.5" "3.5.6" "4.0.0" "latest"; do
